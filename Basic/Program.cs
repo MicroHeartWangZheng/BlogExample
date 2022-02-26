8ec1508acf1ec1c0f0226dc7a2c74b1d396107da
王郑 <418706636@qq.com>
2022-02-26 14:54:51 +0800

    基础知识

---
+++ Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var parent = new Parent();
+            //Console.WriteLine(parent.Name);
+
+            var son = new Son();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basic/Son.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic
+{
+    public class Son : Parent
+    {
+        public override string Name { get; set; } = "1";
+
+
+        public Son() : base()
+        {
+            Console.WriteLine("Son:" + Name);
+        }
+        public Son(string name) : base(name)
+        {
+            this.Name = name;
+        }
+    }
+
+
+    public class Parent
+    {
+
+        public virtual string Name { get; set; } = "2";
+
+        public Parent()
+        {
+            Console.WriteLine("Parent:" + Name);
+        }
+
+        public Parent(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
